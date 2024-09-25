599e3661af95f4d920e154e5204c6a6aaf0a0dcf
tikson <workmessegetb@gmail.com>
2024-09-25 17:42:10 +0300

    3 step

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,14 +1,5 @@
 import os
 import pandas as pd
-"""
-Для вычисления количественного значения Pl ВУ необходимо:
-    – определить полное конечное множество уязвимостей, характерных для данной ИС,
-    – получить лингвистические экспертные оценки от каждого j-го эксперта,
-    – определить причинно-следственные связи между уязвимостями ИС и угрозами ИБ,
-    – вычислить значения Plj_ВУ по оценкам каждого j-го эксперта,
-    – агрегировать полученные значения Pl(j)_ВУ в общую оценку.
-"""
-
 
 def read_excel_to_dataframe(file_path):
     """
@@ -35,8 +26,6 @@ def read_excel_to_dataframe(file_path):
         print(f"Ошибка при чтении файла: {e}")
         return None
 
-
-
 class SecurityIncidentAnalyzer:
     def __init__(self, variant:int, constant_path="Constant", variant_path="Variant_input"):
         """
@@ -50,6 +39,7 @@ class SecurityIncidentAnalyzer:
                  'Уязвимости программного типа',                                            (2.5)
                  'Уязвимости программно-аппаратного типа',                                  (2.6)
                  'Общая классификация угроз ИБ организации',                                (2.7)
+
                  'Взаимосвязи угроз ИБ и уязвимостей физического типа',                     (2.8)
                  'Взаимосвязи угроз ИБ и уязвимостей физического и организационного типа',  (2.9)
                  'Взаимосвязи угроз ИБ и уязвимостей организационного типа',                (2.10)
@@ -65,11 +55,25 @@ class SecurityIncidentAnalyzer:
 
 
         """
+
         self.variant = variant
         self.constant_patch = constant_path
         self.variant_patch = variant_path
-        self.vulnerability_comparison_matrix = pd.DataFrame()
+
+        # df с исходными данными для вариантов
         self.vulnerability_matrix = pd.DataFrame()
+        self.vulnerability_comparison_matrix = pd.DataFrame()
+
+        # словарь с df уязвимостей
+        self.vulnerability_tables = {}
+
+        # Список с уязвимостями, характерными варианту
+        self.list_vulnerability_by_variant = []
+
+        self.defining_input_constant_vulnerability_table()
+        self.defining_input_constant_threat_IS_table()
+        self.defining_input_table()
+
 
     def defining_input_table(self):
         """
@@ -84,7 +88,7 @@ class SecurityIncidentAnalyzer:
             start_index = row.index[0]
             df_trimmed = df.drop('№ варианта', axis=1)
             self.vulnerability_matrix = df_trimmed.iloc[start_index:start_index + 23]
-            self.vulnerability_matrix.columns = ['№ Типа', ' № Группы ', '1', '2', '3', '4', '5', '6','7']
+            self.vulnerability_matrix.columns = ['№ Типа', '№ Группы', '1', '2', '3', '4', '5', '6','7']
             self.vulnerability_matrix = self.vulnerability_matrix.reset_index(drop=True)
 
 
@@ -111,6 +115,181 @@ class SecurityIncidentAnalyzer:
         print("Матрица парного сравнения типов уязвимостей")
         print(self.vulnerability_comparison_matrix.to_markdown(index=False))
 
+    def defining_input_constant_vulnerability_table(self):
+        """
+        Функция инициализирует df уязвимостей. При этом оставляет только нужный контекст от таблиц.
+        'Уязвимости физического типа'               (2.2)
+        'Уязвимости организационного типа'          (2.3)
+        'Уязвимости технического типа'              (2.4)
+        'Уязвимости программного типа'              (2.5)
+        'Уязвимости программно-аппаратного типа'    (2.6)
+        :return:
+        """
+
+        id_type_name_table = {
+            1: ["2.2.xlsx", "Уязвимости физического типа"],
+            2: ["2.3.xlsx", "Уязвимости организационного типа"],
+            3: ["2.4.xlsx", "Уязвимости технического типа"],
+            4: ["2.5.xlsx", "Уязвимости программного типа"],
+            5: ["2.6.xlsx", "Уязвимости программно-аппаратного типа"],
+        }
+        list_columns_to_drop = ["№ группы", "Наименование группы"]
+
+        for id_table, name_table in id_type_name_table.items():
+            path = f"{self.constant_patch}/{name_table[0]}"
+            df = read_excel_to_dataframe(path)
+            check_columns_to_drop = all(column in df.columns for column in list_columns_to_drop)
+            if check_columns_to_drop:
+                df_trimmed = df.drop(list_columns_to_drop, axis=1)
+                df_cleaned = df_trimmed.dropna()
+                df = df_cleaned.reset_index(drop=True)
+
+                print("\n")
+                print(name_table[1])
+                print(df.to_markdown(index=False))
+                self.vulnerability_tables[id_table] = df
+
+
+
+
+
+
+
+
+            # self.vulnerability_tables[id_table] = df
+
+    def defining_input_constant_threat_IS_table(self):
+        """
+        Функция инициализирует df угроз ИБ. При этом оставляет только нужный контекст от таблиц.
+
+        'Взаимосвязи угроз ИБ и уязвимостей физического типа',                     (2.8)
+        'Взаимосвязи угроз ИБ и уязвимостей физического и организационного типа',  (2.9)
+        'Взаимосвязи угроз ИБ и уязвимостей организационного типа',                (2.10)
+        'Взаимосвязи угроз ИБ и уязвимостей организационного и технического типа', (2.11)
+        'Взаимосвязи угроз ИБ и уязвимостей технического типа',                    (2.12)
+        'Взаимосвязи угроз ИБ и уязвимостей технического и программного типа',     (2.13)
+        'Взаимосвязи угроз ИБ и уязвимостей программного типа',                    (2.14)
+        'Взаимосвязи угроз ИБ и уязвимостей программно-аппаратного типа'           (2.15)
+
+        :return:
+        """
+        # ToDo: Переделать таблицы (2.8 - 2.15). Сделать из них 1, условно длинную
+        print(0)
+
+
+
+    def transform_to_inverse_symmetric(self):
+        """
+        Во-первых, необходимо преобразовать матрицу парных сравнений всех уязвимостей
+        (которые приводятся в таблице 2.16 по вариантам)
+        из противоположно симметричной в обратно симметричную.(2.23)
+        :return:
+        """
+        self.vulnerability_comparison_matrix = self.vulnerability_comparison_matrix.map(lambda x: float(x))
+        for i in range(self.vulnerability_comparison_matrix.shape[0]):
+            for j in range(1, self.vulnerability_comparison_matrix.shape[1]):
+                value_i_J = self.vulnerability_comparison_matrix.iat[i, j]
+                if value_i_J > 0:
+                    value_i_J = value_i_J + 1
+                else:
+                    value_i_J = 1 / (1 - value_i_J)
+                self.vulnerability_comparison_matrix.iat[i, j] = float(round(value_i_J, 3))
+
+
+
+        print("\n")
+        print("Матрица парных сравнений, преобразованная по формуле 2.23, в обратно симметричную")
+        print(self.vulnerability_comparison_matrix.to_markdown(index=False))
+
+    def search_vulnerability(self, number_type: int, code:str ):
+        """
+        Функция, которая по заданным параметрам ищет название уязвимости в
+        vulnerability_tables - словарь с df уязвимостей
+        :return:
+        """
+
+        df = self.vulnerability_tables[number_type]
+
+        for _, row in df.iterrows():
+            if row["№ уязвимости"] == code:
+                return row["Наименование уязвимости"]
+
+    def scan_for_vulnerabilities(self):
+        """
+        Функция анализирует матрицу vulnerability_matrix и определяет уязвимости, характерные, для переданного варианта.
+
+        :return:
+        """
+
+        number_type = 0
+
+        for index_df, row in self.vulnerability_matrix.iterrows():
+            if str(row["№ Типа"]) != "nan":
+                number_type = int(row["№ Типа"])
+            number_group = row["№ Группы"]
+            list_number_vulnerability = row[2:].to_dict()
+            for index_dict, value_dict in list_number_vulnerability.items():
+                if value_dict == "+":
+                    code = f"{number_group}.{index_dict}"
+                    name_vulnerability = self.search_vulnerability(number_type, code)
+                    self.list_vulnerability_by_variant.append(
+                        {
+                            "number_type": number_type,
+                            "number_group": number_group,
+                            "code_vulnerability": code,
+                            "name_vulnerability": name_vulnerability
+                        }
+                    )
+
+    def gamma_matrix(self):
+        """
+        Функция создает матрицу гамма.
+        :return:
+        """
+        arr = [i["code_vulnerability"] for i in self.list_vulnerability_by_variant]
+        df = pd.DataFrame(index=arr, columns=arr)
+        df_columns = df.columns.to_list()
+
+        for index, row in df.iterrows():
+            code_row = str(index)
+            code_comparison_matrix_row = code_row.split(".")[0]
+
+            for imdex_column in range(len(row)):
+                code_column = str(df_columns[imdex_column])
+                code_comparison_matrix_column = code_column.split(".")[0]
+                value = self.vulnerability_comparison_matrix.loc[int(code_comparison_matrix_row) - 1, code_comparison_matrix_column] # Костыль, code_comparison_matrix_row это не индекс
+                df.loc[index, code_column] = value
+
+        print("\n")
+        print("Масштабированная матрица парных сравнений")
+        print(df)
+
+    # Теперь бы нам надо построить матрицу взаимодейтсвия угроз и уязвимтостей  №угрозы X №уязвимотсей, характерных для нашего варианта
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def run(self):
-        self.defining_input_table()
+        self.transform_to_inverse_symmetric()
+        self.scan_for_vulnerabilities()
+        self.gamma_matrix()
+
+        # print("\n")
+        #
+        # for i in self.list_vulnerability_by_variant:
+        #     print(i)
+        # print(len(self.list_vulnerability_by_variant))
+
+
