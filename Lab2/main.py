06144867a5927d19750f8b193263112895880902
tikson <workmessegetb@gmail.com>
2024-09-26 13:28:30 +0300

    4 step

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,6 +1,10 @@
 import os
+from time import perf_counter
+
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import cdist, pdist, squareform
+
 
 def read_excel_to_dataframe(file_path):
     """
@@ -57,6 +61,8 @@ class SecurityIncidentAnalyzer:
 
         """
 
+        self.distance_matrix_means = pd.DataFrame()
+        self.distance_matrix = pd.DataFrame()
         self.total_score_matrix = pd.DataFrame()
         self.pk_matrix = pd.DataFrame()
         self.matrix_gamma = pd.DataFrame()
@@ -77,7 +83,6 @@ class SecurityIncidentAnalyzer:
 
         self.defining_input_constant_vulnerability_table()
         self.defining_input_table()
-
 
     def defining_input_table(self):
         """
@@ -301,6 +306,102 @@ class SecurityIncidentAnalyzer:
         print("Матрица интегральных показателей")
         print(self.total_score_matrix)
 
+    def calculate_relative_frequencies(self):
+        """
+        Функция вычисляет относительные частоты возникновения угроз безопасности по
+        оценкам j-го эксперта. (2.33)
+
+        :return:
+        """
+
+        sum_total_score = self.total_score_matrix.sum()
+        arr = []
+        self.total_score_matrix = self.total_score_matrix.to_frame()
+
+        for index, row in self.total_score_matrix.iterrows():
+            value_tsm = row[0] / sum_total_score
+            arr.append(value_tsm)
+        self.total_score_matrix[""] = arr
+        self.total_score_matrix.columns = ["Интегральный показатель ВУ", "Относительные частоты ВУ"]
+
+        print("\n")
+        print("Относительные частоты возникновения угроз безопасности по оценкам j-го эксперта")
+        print(self.total_score_matrix)
+
+    # Анализ относительных частот возникновения угроз ИБ
+    def step_1st(self):
+        """
+        1 шаг. Построим матрицу расстояний между оценками.(2.38)
+        :return:
+        """
+        index = self.total_score_matrix.index.to_list()
+        frequencies_vy = self.total_score_matrix.loc[: ,"Относительные частоты ВУ"]
+        df = pd.concat([frequencies_vy], axis=1)
+        dist_matrix = cdist(df, df, metric='euclidean')
+        self.distance_matrix = pd.DataFrame(dist_matrix, index=index, columns=index)
+
+        print("\n")
+        print("Матрица расстояний между оценками")
+        print(self.distance_matrix)
+
+    def step_2st(self):
+        """
+        2 шаг. Вычислим средние значения расстояний частот возникновения для каждой пары угроз.
+        :return:
+        """
+
+        row_means= self.distance_matrix.mean(axis=1)
+        self.distance_matrix_means = pd.DataFrame(row_means)
+
+        print("\n")
+        print("Средние значения расстояний частот возникновения для каждой пары угроз")
+        print(self.distance_matrix_means)
+
+    def step_3st(self):
+        """
+        3 шаг. Сделаем предварительные привязки потенциальных «одногруппников» для каждой угрозы: возьмём те угрозы, расстояния до
+        которых будет строго меньше среднего расстояния базовой угрозы в
+        группе. Количество таких «одногруппников» может быть любым.
+        Кроме того, одни и те же угрозы могут встречаться в разных группах.
+        :return:
+        """
+        len_column = len(self.distance_matrix.index.to_list())
+        df = pd.DataFrame(index=self.distance_matrix.index.to_list(), columns=range(len_column))
+        for i in range(self.distance_matrix.shape[0]):
+            arr = []
+            for j in range(self.distance_matrix.shape[1]):
+                value_distance_matrix = self.pss_matrix.iat[i, j]
+                value_distance_matrix_means = self.distance_matrix_means.iat[i, 0]
+                if value_distance_matrix < value_distance_matrix_means:
+                    name = self.distance_matrix.columns.to_list()[j]
+                    arr.append(name)
+            arr = np.pad(arr, (0, len_column - len(arr)), mode='constant',  constant_values=np.nan)
+            df.iloc[i] = arr
+
+
+
+        df.columns = [i for i in range(1,28)]
+        print("\n")
+        print("Предварительные привязки по группам угроз")
+        print(df)
+
+    def step_4st(self):
+        """
+        4 шаг. Просмотрим все предварительные группы и выберем из
+        них те угрозы, расстояния до которых от базовой угрозы минимальны.
+        При этом общее количество угроз в группе не должно превышать заданного значения, приблизительно равного отношению общего числа
+        угроз к количеству планируемых групп. Угрозы, которые в процессе
+        просмотра уже были включены в какую-то группу, в другие группы не
+        включаем. Просмотр сделаем в прямом порядке
+
+        :return:
+        """
+
+
+
+
+
+
 
 
 
@@ -326,6 +427,12 @@ class SecurityIncidentAnalyzer:
         self.defining_input_constant_threat_IS_table()
         self.calculate_pk_matrix()
         self.calculate_total_score_matrix()
+        self.calculate_relative_frequencies()
+        print("\n")
+        print("Анализ относительных частот возникновения угроз ИБ")
+        self.step_1st()
+        self.step_2st()
+        self.step_3st()
 
         # print("\n")
         #
